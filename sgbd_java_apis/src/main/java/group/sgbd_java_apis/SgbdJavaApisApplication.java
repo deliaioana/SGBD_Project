7120f2c1b36d6323fa16be0dd3e7f4dca51711a2
Delia-Ioana Oloeriu <deliaoloeriu18@gmail.com>
2022-05-26 21:32:49 +0300

    java project setup

---
+++ sgbd_java_apis/src/main/java/group/sgbd_java_apis/SgbdJavaApisApplication.java
@@ -0,0 +1,13 @@
+package group.sgbd_java_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgbdJavaApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgbdJavaApisApplication.class, args);
+	}
+
+}
